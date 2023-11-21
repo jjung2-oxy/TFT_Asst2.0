67c51be0558518a63b6e139ac84aa645bf64a651
Jordan Jung <jjung2@oxy.edu>
2023-11-21 01:34:31 -0800

    yolo_to_coco changes

--- TrainingFiles/yolo_to_coco.py
+++ TrainingFiles/yolo_to_coco.py
@@ -62,16 +62,24 @@ def yolo_to_coco(input_directory, output_path, image_width, image_height, catego
         json.dump(coco_format, out_file, indent=4)
 
 # Example usage
-input_directory = r"C:\Users\Jorda\OneDrive\Desktop\TFTbot2.0\TrainingFiles\labels"
-output_path = r"C:\Users\Jorda\OneDrive\Desktop\TFTbot2.0\TrainingFiles\instances_default.json"
+input_directory = r"/Users/jordanjung/Desktop/TFTbot2.0/runs/detect/predict/labels/"
+output_path = r"/Users/jordanjung/Desktop/TFTbot2.0/TrainingFiles/instances_default.json"
 image_width = 1440  # Replace with your actual image width
 image_height = 720  # Replace with your actual image height
 
 # Categories from your example JSON
-categories = [
-    {"id": 1, "name": "Cassiopeia", "supercategory": ""},
-    {"id": 2, "name": "Annie", "supercategory": ""},
-    # ... (add other categories as needed)
-]
+category_names = {
+    0: 'Ahri', 1: 'Akali K-DA', 2: 'Akali True-DMG', 3: 'Amumu', 4: 'Annie', 5: 'Aphelios', 6: 'Bard', 
+    7: 'Blitzcrank', 8: 'Caitlyn', 9: 'Cassiopeia', 10: 'Corki', 11: 'Ekko', 12: 'Evelynn', 13: 'Ezreal', 
+    14: 'Garen', 15: 'Gnar', 16: 'Gragas', 17: 'Illaoi', 18: 'Jax', 19: 'Jhin', 20: 'Jinx', 21: "K'Sante", 
+    22: "Kai'Sa", 23: 'Katarina', 24: 'Kayle', 25: 'Kayn', 26: 'Kennen', 27: 'Lillia', 28: 'Lucian', 
+    29: 'Lulu', 30: 'Lux', 31: 'Miss Fortune', 32: 'Mordekaiser', 33: 'Nami', 34: 'Neeko', 35: 'Olaf', 
+    36: 'Pantheon', 37: 'Poppy', 38: 'Qiyana', 39: 'Riven', 40: 'Samira', 41: 'Senna', 42: 'Seraphine', 
+    43: 'Sett', 44: 'Sona', 45: 'Tahm Kench', 46: 'Taric', 47: 'Thresh', 48: 'Twisted Fate', 49: 'Twitch', 
+    50: 'Urgot', 51: 'Vex', 52: 'Vi', 53: 'Viego', 54: 'Yasuo', 55: 'Yone', 56: 'Yorick', 57: 'Zac', 
+    58: 'Zed', 59: 'Ziggs'
+}
+
+categories = [{"id": idx + 1, "name": name, "supercategory": ""} for idx, name in category_names.items()]
 
 yolo_to_coco(input_directory, output_path, image_width, image_height, categories)
